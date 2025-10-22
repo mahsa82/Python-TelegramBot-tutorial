d761100caca08636462f4ba8d91f6415074214f3
mahsa82 <mahsa.alizade8271@gmail.com>
2025-10-22 15:03:30 +0330

    adding a feature of all text and hello text

--- core/main.py
+++ core/main.py
@@ -1,11 +1,15 @@
 import telebot
 import os
 import json
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 API_TOKEN = os.environ.get("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
-#Handle '/start' and '/help'
+# Handle '/start' and '/help'
 @bot.message_handler(commands=['help','start'])
 def send_welcome(message):
     # bot.reply_to(message,"""\
@@ -13,4 +17,26 @@ def send_welcome(message):
     #     """)
     bot.send_message(message.chat.id,json.dumps(message.chat.__dict__,indent=4,ensure_ascii=False))
 
+# Handels all sent documents and audio files
+@bot.message_handler(content_types =['document','audio'])
+def handle_docs_audio(message):
+    if message.content_type == "document":
+        print("Its a document.")
+    elif message.content_type == 'audio':
+        print("Its an audio")
+
+
+# Handles all messages for which the lambda returns True
+
+
+@bot.message_handler(func=lambda message: message.text == "hello")
+def handle_text_doc(message):
+    bot.send_message(message.chat.id, "Hi dear, I hope you the best day!")
+    
+@bot.message_handler(func=lambda message:True)
+def handle_all_text(message):
+    bot.send_message(message.chat.id, "Thank you for your suggestion.")
+    
+# start polling        
 bot.infinity_polling()
+    
